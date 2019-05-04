3fa28e4b233109a6c95f7983a79cf477bfc958a7
Julian Rüth <julian.rueth@fsfe.org>
2019-05-05 00:19:38 +0200

    Fix build for Jupyter Lab

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,10 @@ const rules = [
     appendTsSuffixTo: [/\.vue$/],
   } },
   { test: /\.js$/, loader: 'source-map-loader' },
-  { test: /\.css$/, use: ['vue-style-loader', { loader: 'css-loader', options: { modules: true } }]},
-  { test: /\.scss$/, use: ['vue-style-loader', { loader: 'css-loader', options: { modules: true } } , 'sass-loader']},
+  { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+  { test: /\.scss$/, use: ['vue-style-loader', 'css-loader', 'sass-loader']},
   { test: /\.css$/, use: ['style-loader', 'css-loader']},
-  { test: /\.vue$/, use: ['vue-loader']}
+  { test: /\.vue$/, use: ['vue-loader']},
   { test: /\.svg$/, use: ['svg-url-loader']},
 ];
 
