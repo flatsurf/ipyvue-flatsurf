4eff9c957481697f11fc117ed8a6114af9421b0a
Julian Rüth <julian.rueth@fsfe.org>
2024-09-25 23:19:15 +0300

    bumped version to 0.6.2

--- ipyvue_flatsurf/__init__.py
+++ ipyvue_flatsurf/__init__.py
@@ -49,8 +49,8 @@ from ipyvue_flatsurf.widget import Widget
 Widget  # silence pyflakes
 
 
-version_info = (0, 6, 1)
-__version__ = "0.6.1"
+version_info = (0, 6, 2)
+__version__ = "0.6.2"
 
 
 def _jupyter_labextension_paths():
